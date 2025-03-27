c30e48fccacbfae9a25ac30942e569f9c8b1a91e
Vladimir Kromm <vkromm@gmail.com>
2025-03-27 12:28:24 +0100

    infinite scroll implementation

--- src/actions/product.ts
+++ src/actions/product.ts
@@ -7,8 +7,8 @@ import { Product } from '@/db/product';
 import { ProductCompany } from '@/db/product-company';
 import {DEFAULT_PRODUCTS_LIMIT} from '@/settings';
 
-export async function getProductsByCompanyId(companyId: number, page: number = 1, limit: number = DEFAULT_PRODUCTS_LIMIT, search: string = '') {
-    const offset = (page - 1) * limit;
+export async function getProductsByCompanyId(companyId: number, page: number = 1, search: string = '') {
+    const offset = (page - 1) * DEFAULT_PRODUCTS_LIMIT;
 
     // const {count, rows: links} = await ProductCompany.findAndCountAll({where: {companyId}, limit, offset})
     // const products = await Product.findAll({where: {id: {[Op.in]: links.map(link=>link.productId)}}})
@@ -16,19 +16,26 @@ export async function getProductsByCompanyId(companyId: number, page: number = 1
     if (search) {
         productWhere.name = { [Op.substring]: search }; // Поиск по подстроке
     }
-    
+
     const {count, rows: links} = await ProductCompany.findAndCountAll({
         logging: false,
-        where: {companyId}, limit, offset,
+        where: {companyId},
+        limit: DEFAULT_PRODUCTS_LIMIT,
+        offset,
         include: [
             { model: Product, as: 'product', where: productWhere }
         ],
         order: [[{model: Product, as: 'product'}, 'name', 'ASC']]
     })
-    
+
+    console.log('getProductsByCompanyId', offset, links.length, count, offset + links.length <= count)
+
     return {
-        products: links.map(link=>({id: link.product?.id, name: link.product?.name})),
-        hasMore: offset + links.length <count,
+        products: links.map(link=>{
+            if (!link.product) throw new Error('Not correct used include')
+            return { id: link.product.id, name: link.product.name }
+        }),
+        hasMore: offset + links.length < count,
         count: 2,
     };
 }
--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { useState, useEffect, useRef, ChangeEvent } from 'react';
+import {useState, useEffect, useRef} from 'react';
 import { Input } from '@heroui/input';
-import {Select, SelectItem} from '@heroui/select';
 
 import {getProductsByCompanyId} from '@/actions/product';
-import {PRODUCTS_LIMIT_OPTIONS, DEFAULT_PRODUCTS_LIMIT} from '@/settings';
+import {debounce} from '@/helper';
 
 interface Product {
   id: number;
@@ -20,33 +19,53 @@ interface ProductListProps {
 
 export default function ProductList ({ companyId, initialProducts, initialHasMore }: ProductListProps) {
     const [products, setProducts] = useState<Product[]>([]);
-    const [page, setPage] = useState(2);
+    const [page, setPage] = useState(1);
     const [hasMore, setHasMore] = useState(initialHasMore);
     const [isLoading, setIsLoading] = useState(false);
     const [search, setSearch] = useState('');
-    const [limit, setLimit] = useState(DEFAULT_PRODUCTS_LIMIT);
 
     const containerRef = useRef<HTMLDivElement | null>(null);
     const lastProductRef = useRef<HTMLDivElement | null>(null);
-    
+    const intersectionObserver = useRef<IntersectionObserver | null>(null)
 
     useEffect(() => {
         setProducts(initialProducts);
     }, [initialProducts]);
 
-    const onChangeLimit = (e) => {
-        setLimit(e.target.value);
-    }
+    useEffect(() => {
+        if (page===1){
+            // initial products were rendered from server
+            return
+        }
+        loadMore()
+    }, [page, search]);
 
-    const onInputSearch = async (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
+    useEffect(() => {
+        intersectionObserver.current = new IntersectionObserver(
+            async (entries) => {
+                if (entries[0]?.isIntersecting) {
+                    handleScrollIntersectionDebounced()
+                }
+            },
+            {
+                root: containerRef.current,
+                rootMargin: '0px 0px 200px 0px',
+                threshold: 0
+            }
+        );
+        if (lastProductRef.current)
+            intersectionObserver.current.observe(lastProductRef.current)
+        return () => {
+            intersectionObserver.current?.disconnect()
+        };
+    }, [products.length]);
+
+    const loadMore = async () => {
         setIsLoading(true);
         try {
-            const data = await getProductsByCompanyId(companyId, page, limit, e.target.value)
-
-            setProducts(data.products);
+            const data = await getProductsByCompanyId(companyId, page, search);
+            setProducts(prev=>[...prev, ...data.products]);
             setHasMore(data.hasMore);
-            setPage(1);
         } catch (error) {
             console.error('Ошибка загрузки:', error);
         } finally {
@@ -54,44 +73,14 @@ export default function ProductList ({ companyId, initialProducts, initialHasMor
         }
     }
 
-    const loadProducts = async (searchParam: string) => {      
-        if (!hasMore || isLoading) return;
-
-        setIsLoading(true);
-        try {          
-            const data = await getProductsByCompanyId(companyId, page, 20, searchParam)
-
-            setProducts(prev => [...prev, ...data.products]);
-            setHasMore(data.hasMore);
-            setPage(prev => prev + 1);
-        } catch (error) {
-            console.error('Ошибка загрузки:', error);
-        } finally {
-            setIsLoading(false);
+    const handleScrollIntersectionDebounced = debounce(async ()=>{
+        if (isLoading) {
+            return
         }
-    };
-
-    // Наблюдатель за последним элементом
-    useEffect(() => {      
-        setTimeout(() => {
-            const observer = new IntersectionObserver(
-                async (entries) => {      
-                    if (entries[0]?.isIntersecting && hasMore && !isLoading) {
-                        await loadProducts(search);
-                    }
-                },
-                {
-                    root: containerRef.current,
-                    rootMargin: '0px 0px 200px 0px',
-                    threshold: 0
-                }
-            );
-
-            if (lastProductRef.current) observer.observe(lastProductRef.current);
-
-            return () => observer.disconnect();
-        })
-    }, [hasMore, page, isLoading]);
+        if (hasMore){
+            setPage(page=>page+1)
+        }
+    }, 500)
 
     return (
         <>
@@ -100,13 +89,16 @@ export default function ProductList ({ companyId, initialProducts, initialHasMor
                     className="mb-5"
                     label="Поиск"
                     type="text"
-                    onInput={onInputSearch}
-                /> <Select className="max-w-xs" label="Продуктов на страницу" onChange={onChangeLimit}>
-                    {PRODUCTS_LIMIT_OPTIONS.map((option: { key: string; label: string; }) => (
-                        <SelectItem key={option.key}>{option.label}</SelectItem>
-                    ))}
-                </Select>
+                    onInput={(e) => {
+                        setProducts([])
+                        setPage(1)
+                        setSearch(e.target.value)
+                    }}
+                />
             </div>
+            <div>Search: {search}</div>
+            <div>Page: {page}. Hasmore: {String(hasMore)}</div>
+            <div>Products number: {products.length}</div>
             {products.length > 0 && <div
                 ref={containerRef}
                 className="h-[600px] overflow-y-auto border border-gray-200 rounded-lg p-4"
@@ -118,11 +110,12 @@ export default function ProductList ({ companyId, initialProducts, initialHasMor
                                 key={product.id}
                                 className="p-4 bg-white rounded-lg shadow-sm border border-gray-100"
                             >
-                                <p className="text-gray-800 font-medium">idx: {index}: id: {product.id}. {product.name}</p>
+                                <p className="text-gray-800 font-medium">idx: {index}:
+                                    id: {product.id}. {product.name}</p>
                             </div>
                         )
                     })}
-                    <div ref={lastProductRef}></div>
+                    <div ref={lastProductRef}>last</div>
                     {isLoading && (
                         <div className="p-4 text-center text-gray-500">
                             Загрузка...
